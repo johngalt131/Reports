acde3b22d855aec64760fff8859bcf3baba4f4fe
Travis Staley <travisstaley@Travis-Staleys-MacBook-Pro.local>
2012-03-03 07:27:24 -0500

    Submitting some files before I try running prep.sh for the first time in a while.

--- FinancialRecordList.cpp
+++ FinancialRecordList.cpp
@@ -46,198 +46,202 @@ void FinancialRecordList::ReadData(){
 	switch(i){
 	  
 	  // BEGINCASE
-	  // This is automatically generated code
-	  // Do Not Edit
-	case DATE:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_date(temp_string);
-	  }
-	  break;
-	}
-	case CLEARED_VAL:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_cleared_val(temp_string);
-	  }
-	  break;
-	}
-	case DESCRIPTION:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_description(temp_string);
-	  }
-	  break;
-	}
-	case DEBIT_VAL:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_debit_val(temp_string);
-	  }
-	  break;
-	}
-	case NUM:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_num(temp_string);
-	  }
-	  break;
-	}
-	case CATEGORY:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_category(temp_string);
-	  }
-	  break;
-	}
-	case BUDGET_CATEGORY:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_budget_category(temp_string);
-	  }
-	  break;
-	}
-	case NOTES:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_notes(temp_string);
-	  }
-	  break;
-	}
-	case ACCOUNT:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_account(temp_string);
-	  }
-	  break;
-	}
-	case DISCRETIONARY:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_discretionary(temp_string);
-	  }
-	  break;
-	}
-	case CREDIT_VAL:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_credit_val(temp_string);
-	  }
-	  break;
-	}
-	case ORDER:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_order(temp_string);
-	  }
-	  break;
-	}
-	case REPAY:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_repay(temp_string);
-	  }
-	  break;
-	}
-	case PENDING_TOTAL:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_pending_total(temp_string);
-	  }
-	  break;
-	}
-	case RECONCILED_VAL:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_reconciled_val(temp_string);
-	  }
-	  break;
-	}
-	case CLEARED_BALANCE:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_cleared_balance(temp_string);
-	  }
-	  break;
-	}
-	case RECONCILED_BALANCE:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_reconciled_balance(temp_string);
-	  }
-	  break;
-	}
-	case TOTAL_DISCRETIONARY:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_total_discretionary(temp_string);
-	  }
-	  break;
-	}
-	case DISCRETIONARY_AMT:{
-	  const char *temp = child->GetText();
-	  std::string temp_string;
-	  if( temp ){ 
-	    temp_string = temp;
-	    //free(temp);
-	    record->set_discretionary_amt(temp_string);
-	  }
-	  break;
-	}
+// BEGINCASE
+// This is automatically generated code
+// Do Not Edit
+case DATE:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_date(temp_string);
+}
+break;
+}
+case CLEARED_VAL:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_cleared_val(temp_string);
+}
+break;
+}
+case DESCRIPTION:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_description(temp_string);
+}
+break;
+}
+case DEBIT_VAL:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_debit_val(temp_string);
+}
+break;
+}
+case NUM:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_num(temp_string);
+}
+break;
+}
+case CATEGORY:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_category(temp_string);
+}
+break;
+}
+case BUDGET_CATEGORY:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_budget_category(temp_string);
+}
+break;
+}
+case NOTES:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_notes(temp_string);
+}
+break;
+}
+case ACCOUNT:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_account(temp_string);
+}
+break;
+}
+case DISCRETIONARY:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_discretionary(temp_string);
+}
+break;
+}
+case CREDIT_VAL:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_credit_val(temp_string);
+}
+break;
+}
+case ORDER:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_order(temp_string);
+}
+break;
+}
+case REPAY:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_repay(temp_string);
+}
+break;
+}
+case PENDING_TOTAL:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_pending_total(temp_string);
+}
+break;
+}
+case RECONCILED_VAL:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_reconciled_val(temp_string);
+}
+break;
+}
+case CLEARED_BALANCE:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_cleared_balance(temp_string);
+}
+break;
+}
+case RECONCILED_BALANCE:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_reconciled_balance(temp_string);
+}
+break;
+}
+case TOTAL_DISCRETIONARY:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_total_discretionary(temp_string);
+}
+break;
+}
+case DISCRETIONARY_AMT:{
+const char *temp = child->GetText();
+std::string temp_string;
+if( temp ){ 
+temp_string = temp;
+free(temp);
+NAMEOFCLASSset_discretionary_amt(temp_string);
+}
+break;
+}
+// case statements
+
+// ENDCASE
 	  //ENDCASE
 	defualt:
 	  break;
